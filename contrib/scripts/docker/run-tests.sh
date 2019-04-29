326e9e083518d4277bc99a2186217100a06ca492
Benedikt Heine <bebe@bebehei.de>
2019-04-29 15:10:31 +0200

    Remove last copy layer
    Signed-off-by: Benedikt Heine <bebe@bebehei.de>

--- contrib/scripts/docker/run-tests.sh
+++ contrib/scripts/docker/run-tests.sh
@@ -3,6 +3,6 @@
 apt-get update
 apt-get install -y --no-install-recommends tesseract-ocr-deu
 
-$MAYAN_PIP_BIN install -r $DOCKER_ROOT/requirements-testing.txt
+$MAYAN_PIP_BIN install -r $DOCKER_ROOT/testing-base.txt
 
 $MAYAN_BIN test --mayan-apps --settings=mayan.settings.testing
